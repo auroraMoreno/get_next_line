21a6f8519d9f65c02db5a7b14a808465f5fe3758
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2024-01-17 20:55:13 +0100

    comienzo gnl lee archivo

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,36 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 11:36:49 by aumoreno          #+#    #+#             */
-/*   Updated: 2024/01/16 17:11:59 by aumoreno         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:57:58 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#define BUFFER_SIZE 1024
 
 char	*get_next_line(int fd)
 {
-	//read(fd,,);
+    char *line;
+    char *buffer;
+    // char static *stash;
+    ssize_t bytes_read;
+    
+    bytes_read = 0;
+    buffer = malloc(sizeof(char) *(BUFFER_SIZE + 1));
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+    line = malloc(bytes_read + 1);
+    
+    ft_memcpy(line, buffer, bytes_read);
+    line[bytes_read] = '\0';
+    return line;
 }
 
 int main()
 {
     int fd = open("prueba.txt",O_RDWR);
     char* result = get_next_line(fd);
+    printf("%s", result);
+    free(result);
     return (0);
 }
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aumoreno <aumoreno@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 11:37:07 by aumoreno          #+#    #+#             */
-/*   Updated: 2024/01/16 11:37:08 by aumoreno         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:37:57 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (dst == 0 && src == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
